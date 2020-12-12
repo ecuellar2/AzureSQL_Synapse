7cbd94fc1d4d1170ae1ca488b4aa828e79e49259
Edgar <ecuellar2@yahoo.com>
2020-12-12 15:53:27 -0600

    Update synapse_scripts.sql

--- synapse_scripts.sql
+++ synapse_scripts.sql
@@ -140,16 +140,7 @@ WHERE 1 = 1
 GROUP BY sm.name, tb.name
 order by 3
 ====================================
---Synapse
--- create credentials for container using sql on demand
-IF EXISTS (SELECT * FROM sys.credentials WHERE name = 'https://xx.dfs.core.windows.net/container')
-DROP CREDENTIAL [https://xx.dfs.core.windows.net/container]
-
-CREATE CREDENTIAL [https://xx.dfs.core.windows.net/container]
-WITH IDENTITY='SHARED ACCESS SIGNATURE',  
-SECRET = '?sv=%3D'
-
---OPENROWSET WITHOUT data_source, right click sql on demand
+--Synapse OPENROWSET WITHOUT data_source, right click sql on demand
 CREATE CREDENTIAL [https://<storage_account>.dfs.core.windows.net/<container>] WITH IDENTITY='Managed Identity'
 select * from sys.credentials
 
@@ -161,4 +152,21 @@ select * from sys.database_scoped_credentials
 select * from sys.external_file_formats 
 select * from sys.external_data_sources
 select * from sys.external_tables
+
+IF EXISTS (SELECT * FROM sys.credentials WHERE name = 'https://xx.dfs.core.windows.net/container')
+DROP CREDENTIAL [https://xx.dfs.core.windows.net/container]
+CREATE CREDENTIAL [https://xx.dfs.core.windows.net/container] WITH IDENTITY='SHARED ACCESS SIGNATURE',  SECRET = '?sv=%3D'
+
+CREATE EXTERNAL DATA SOURCE [storage_acct]  WITH (TYPE = HADOOP, LOCATION = N'abfss://container@accountname.dfs.core.windows.net',  CREDENTIAL = [mi_cred])
+
+CREATE EXTERNAL TABLE dbo.table_name
+( [id] [smallint] NULL,
+[value]  [varchar](50) NULL)
+WITH ( LOCATION = 'parquet/testdata/',
+DATA_SOURCE = [storage_accta],
+FILE_FORMAT = [ParquetFormat] );
+
+CREATE VIEW name_vw AS SELECT * FROM 
+OPENROWSET(BULK '/parquet/testdata/*.parquet', DATA_SOURCE = 'accountname',FORMAT='PARQUET') AS [r];
+
 
