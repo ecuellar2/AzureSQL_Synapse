53cf26ad716c18f7be1e28961d9749dcc2ec43bd
Edgar <ecuellar2@yahoo.com>
2021-03-13 23:18:18 -0600

    Update synapse_db_workloadgrp_setup.sql

--- synapse_db_workloadgrp_setup.sql
+++ synapse_db_workloadgrp_setup.sql
@@ -105,6 +105,22 @@ MIN_PERCENTAGE_RESOURCE = 15
 , QUERY_EXECUTION_TIMEOUT_SEC = 0
 );
 
+-- View Workload Groups
+SELECT group_id
+, name
+, importance
+, min_percentage_resource
+, parallelism = min_percentage_resource / request_min_resource_grant_percent
+, cap_percentage_resource
+, request_min_resource_grant_percent
+, request_max_resource_grant_percent
+, query_execution_timeout_sec
+, query_wait_timeout_sec
+FROM sys.workload_management_workload_groups;
 
+-- View Workload Classifiers
 
+SELECT WC.classifier_id, WC.name, WC.group_name, WC.importance, WC.is_enabled, CD.classifier_type, CD.classifier_value
+FROM sys.workload_management_workload_classifiers WC
+INNER JOIN sys.workload_management_workload_classifier_details CD ON WC.classifier_id = CD.classifier_id;
 
